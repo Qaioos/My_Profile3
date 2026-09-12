e8e7cdd4ed5ff768603737337e7f32a29b0c9872
Qaioos <legend442005@gmail.com>
2026-09-12 13:53:42 +0300

    Add  contact Components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Running from './Components/Runnung/Running'
 
 import Footer from './feat/Footer/Footer'
 import AboutIdentity from './feat/Landing/components/About_Identty/AboutIdentity'
+import Contact from './feat/Landing/components/Contact/Contact'
 import Expedition from './feat/Landing/components/Expedition/Expedition'
 import Projects from './feat/Landing/components/FeatureWork/Projects'
 import Hero from './feat/Landing/components/Hero/Hero'
+import InterActivTech from './feat/Landing/components/InterActivTech/InterActivTech'
 import Header from './Layout/Headers/Header'
 
 function App() {
@@ -15,9 +17,11 @@ function App() {
     <Header/>
     <Hero/>
     <Running/>
-    <Projects/>
     <AboutIdentity/>
+    <InterActivTech/>
+    <Projects/>
     <Expedition/> 
+    <Contact/>
     <Footer/>
     </>
   )
